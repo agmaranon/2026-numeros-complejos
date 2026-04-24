2d9a4500d6f3666602aaf820466e0e3219eb88da
agmaranon <a.maranon@alumno.etec.um.edu.ar>
2026-04-24 10:33:10 -0300

    numeros

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,15 @@
+package org.example
+import kotlin.random.Random
+
+class Complejo {
+    private var real: Int = 0
+    private var imaginario: Int = 0
+
+    fun ponerValor(real: Int, imaginario: Int): String {
+        return  "($real, $imaginario)"
+    }
+}
+
+fun main() {
+    println(Complejo().ponerValor(Random.nextInt(1, 10), Random.nextInt(1, 10)))
+}
