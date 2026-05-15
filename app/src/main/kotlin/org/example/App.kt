773a1ea61e1e1ecf9a8b635c1642481e9dfad697
agmaranon <a.maranon@alumno.etec.um.edu.ar>
2026-05-15 09:05:45 -0300

    cuentas_nums_complejos

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,16 +1,53 @@
 package org.example
-import kotlin.random.Random
 
-class Complejo {
-    private var real: Int = 0
-    private var imaginario: Int = 0
+class Complejo( private var real: Double = 0.0, private var imag: Double = 0.0) {
 
-    fun ponerValor(real: Int, imaginario: Int): String {
-        return  "($real, $imaginario)"
+    fun mostrar(){
+        println(this.toString())
+    }
+    override fun toString():String{
+        return "(${real},${imag})"
+    }
+    fun sumar(otro: Complejo): Complejo {
+        return Complejo(
+            real + otro.real,
+            imag + otro.imag
+        )
+    }
+    fun restar(otro: Complejo): Complejo {
+        return Complejo(
+            real - otro.real,
+            imag - otro.imag
+        )
+    }
+    fun multiplicar(otro: Complejo): Complejo {
+        val r = real * otro.real - imag * otro.imag
+        val i = real * otro.imag + imag * otro.real
+        return Complejo(r, i)
+    }
+    fun dividir(otro: Complejo): Complejo {
+        val denom = otro.real * otro.real + otro.imag * otro.imag
+        val r = (real * otro.real + imag * otro.imag) / denom
+        val i = (imag * otro.real - real * otro.imag) / denom
+        return Complejo(r, i)
     }
 }
 
 fun main() {
-    println(Complejo().ponerValor(Random.nextInt(1, 10), Random.nextInt(1, 10)))
-    println ("hola mundo")
+    var complejo: Complejo
+    complejo= Complejo(3.0, 4.0)
+    println("${complejo.toString()}")
+    var c2:Complejo= Complejo(1.0, 2.0)
+    var c3:Complejo
+    c3 = complejo.sumar(c2)
+    c3.mostrar()
+    var c4:Complejo
+    c4 = complejo.restar(c2)
+    c4.mostrar()
+    var c5:Complejo
+    c5 = complejo.multiplicar(Complejo(3.0, 4.0))
+    c5.mostrar()
+    var c6:Complejo
+    c6 = complejo.dividir(Complejo(3.0, 4.0))
+    c6.mostrar()
 }
